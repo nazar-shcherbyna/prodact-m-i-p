0f738cc0a4fd41708bd1fed930e026a3b0d7a3dd
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2021-02-06 18:27:49 +0200

    create design tatyana

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ function initMap() {
         lng: 37.55897540422409 
     };
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
+      zoom: 15,
       center: myLatLng,
     });
     var iconBase = 'img/';
@@ -39,22 +39,62 @@ function initMap() {
     });
 }
 
-document.querySelector('.humburger').addEventListener('click', function(e) {
+document.querySelector('.hamburger__btn').addEventListener('click', function(e) {
     e.preventDefault();
     if (this.classList.contains('is-active')) {
         this.classList.remove('is-active');
-        document.querySelector('#menu').classList.remove('nav-active');
-        document.querySelector('.mip__hamburger').classList.remove('body-active');
+        document.querySelector('.hamburger__menu').classList.remove('nav-active');
+        document.querySelector('.hamburger').classList.remove('body-active');
     }
     else {
         this.classList.add('is-active');
-        document.querySelector('#menu').classList.add('nav-active');
-        document.querySelector('.mip__hamburger').classList.add('body-active');
+        document.querySelector('.hamburger__menu').classList.add('nav-active');
+        document.querySelector('.hamburger').classList.add('body-active');
     }
 })
 
-let close = document.getElementsByClassName('hum-link');
+let close = document.getElementsByClassName('ham-link');
 
 function closeMenu(){
-    document.querySelector('.humburger').click();   
+    document.querySelector('.hamburger__btn').click();   
 }
+
+// work with logo-name
+
+let mipName = document.querySelector('.header__h1');
+let mipLittleName = document.querySelector('.header__little-h');
+
+mipName.addEventListener('click', () => {
+  console.log(mipName.getBoundingClientRect().bottom)
+})
+
+function showName(elem, showElem) {
+  document.addEventListener('scroll', function () {
+    if (elem.getBoundingClientRect().bottom <= 0) {
+      showElem.classList.add('show');
+    }
+    if (elem.getBoundingClientRect().bottom > 0) {
+      showElem.classList.remove('show');
+    }
+  })
+}
+
+showName(mipName, mipLittleName);
+
+// document.addEventListener('scroll', function showLittleName() {
+//   if (mipName.getBoundingClientRect().bottom <= 20) {
+//     mipLittleName.classList.add('show');
+//   }
+//   if (mipName.getBoundingClientRect().bottom > 20) {
+//     mipLittleName.classList.remove('show');
+//   }
+// })
+
+// document.addEventListener('scroll', function showLittleName() {
+//   if (mipName.getBoundingClientRect().bottom <= 20) {
+//     mipHumName.classList.add('show');
+//   }
+//   if (mipName.getBoundingClientRect().bottom > 20) {
+//     mipHumName.classList.remove('show');
+//   }
+// })
